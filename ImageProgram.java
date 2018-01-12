a22d12262edf1b3eb788949a88140a589fc1b45d
Majd <majdtahan@hotmail.com>
2018-01-11 23:53:32 -0500

    ImageProgram (Third Question of First Assignment)

---
+++ ImageProgram.java
@@ -0,0 +1,31 @@
+import java.util.Random;
+
+public class ImageProgram {
+
+    public static void main(String[] args) {
+        Random rand = new Random();
+        rand.nextBoolean();
+
+
+        for (int i = 0; i < 121; i++) {
+            Boolean n = rand.nextBoolean();
+
+            if (n == true)
+                System.out.print("0");
+
+            else{
+                System.out.print("1");
+
+                //for (int j=0; j<11; j++);
+
+        }
+    }
+
+
+
+
+
+    }
+
+
+}
